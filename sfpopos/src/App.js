60c060b7c29f788cb4f93a199100f7c0f129ee51
Josh Faigan <djjoker13@gmail.com>
2023-02-02 15:44:19 -0500

    routes complete

--- sfpopos/src/App.js
+++ sfpopos/src/App.js
@@ -1,14 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import Title from './Title';
-import POPOSList from './POPOSList';
-import Footer from './Footer';
+
 
 function App() {
   return (
     <div className="App">
       <Title />
-      <POPOSList />
-      <Footer />
+      <Outlet />
     </div>
   );
 }
